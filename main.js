4ef773e25f27b5ce7e28381b26159af6244c7d43
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 02:32:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// part 9 //question 13/ transpotration
+var transpotration = ["car", "bus", "bike", "plane"];
+for (var i = 0; i < transpotration.length; i++) {
+    console.log("I would like to own a " + transpotration[i]);
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+// part 9 //question 13/ transpotration
+
+let transpotration: string[] = ["car", "bus","bike","plane"];
+for(let i=0; i<transpotration.length; i++){
+    console.log(`I would like to own a ` + transpotration[i]);
+
+}
